5e4d10045835f39c468454b76c9ef0053f8022d7
Hannah Wolfson <128550078+hanwol525@users.noreply.github.com>
2023-07-06 14:31:01 -0500

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   try {
     const getTags = await Tag.findAll({
       include: [{ model: Product }]
@@ -16,7 +15,8 @@ router.get('/', async (req, res) => {
   };
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => { 
+  // find a single tag by its `id`
   try {
     const getTag = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -28,18 +28,16 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
 });
 
 router.post('/', async (req, res) => {
+  // create a new tag
   try {
     const createTag = await Tag.create(req.body);
     res.status(200).json(createTag);
   } catch (err) {
     res.status(500).json(err);
-  }
-  // create a new tag
+  };
 });
 
 router.put('/:id', async (req, res) => {
